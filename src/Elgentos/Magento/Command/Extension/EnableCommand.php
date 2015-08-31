06912728374886506dd050b277016cf0b7298f3a
Andre Flitsch <andre@pixelperfect.at>
2015-08-31 08:23:25 +0200

    Added name argument to command, and made select optional. Check is made for a valid module name. Also added check for previously disabled/enabled module and output message indicating module status, if previously enabled/disabled

--- src/Elgentos/Magento/Command/Extension/EnableCommand.php
+++ src/Elgentos/Magento/Command/Extension/EnableCommand.php
@@ -4,50 +4,79 @@ namespace Elgentos\Magento\Command\Extension;
 
 use N98\Magento\Command\AbstractMagentoCommand;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class EnableCommand extends AbstractMagentoCommand
 {
+
     protected function configure()
     {
-      $this
-          ->setName('extension:enable')
-          ->setDescription('Enable an extension that was disabled through extension:disable')
-      ;
+        $this
+            ->setName('extension:enable')
+            ->setDescription('Enable an extension that was disabled through extension:disable')
+            ->addArgument('name', InputArgument::OPTIONAL, 'The full module name e.g. Namespace_Module');
     }
 
-   /**
-    * @param \Symfony\Component\Console\Input\InputInterface $input
-    * @param \Symfony\Component\Console\Output\OutputInterface $output
-    * @return int|void
-    */
+    /**
+     * @param \Symfony\Component\Console\Input\InputInterface   $input
+     * @param \Symfony\Component\Console\Output\OutputInterface $output
+     *
+     * @return int|void
+     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $this->detectMagento($output);
         if ($this->initMagento()) {
             $dialog = $this->getHelper('dialog');
-            $moduleDir = $this->getApplication()->getMagentoRootFolder() . DS . 'app' . DS . 'etc' . DS . 'modules';
+            $moduleDir = $this->getApplication()->getMagentoRootFolder().DS.'app'.DS.'etc'.DS.'modules';
             $moduleFiles = scandir($moduleDir);
             $moduleFilenames = $moduleNames = array();
-            foreach($moduleFiles as $moduleFile) {
-                if(strtolower(substr($moduleFile,-9)) == '.disabled') {
-                    $xml = simplexml_load_file($moduleDir . DS . $moduleFile);
-                    $keys = array_keys((array)$xml->modules);
+            $enabledModuleFilenames = $enabledModuleNames = array();
+            foreach ($moduleFiles as $moduleFile) {
+                if (strtolower(substr($moduleFile, -9)) == '.disabled') {
+                    $xml = simplexml_load_file($moduleDir.DS.$moduleFile);
+                    $keys = array_keys((array) $xml->modules);
                     $moduleNames[] = $keys[0];
-                    $moduleFilenames[] = $moduleFile; 
+                    $moduleFilenames[] = $moduleFile;
+                }
+
+                if (strtolower(substr($moduleFile, -4)) == '.xml') {
+                    $xml = simplexml_load_file($moduleDir.DS.$moduleFile);
+                    $keys = array_keys((array) $xml->modules);
+                    $enabledModuleNames[] = $keys[0];
+                    $enabledModuleFilenames[] = $moduleFile;
                 }
             }
-            
-            $moduleIndex = $dialog->select(
-                $output,
-                'Select extension to enable',
-                $moduleNames,
-                0
-            );
-            
-            exec('mv ' . $moduleDir . DS . $moduleFilenames[$moduleIndex] . ' ' . $moduleDir . DS . str_replace('.disabled','',$moduleFilenames[$moduleIndex]));
-            
-            $output->writeln('<info>Enabled ' . $moduleNames[$moduleIndex] . '</info>');
+
+            $enableModule = false;
+            $name = $input->getArgument('name');
+            if (!$name) {
+                $moduleIndex = $dialog->select(
+                    $output,
+                    'Select extension to enable',
+                    $moduleNames,
+                    0
+                );
+            } else {
+                if (array_search($name, $moduleNames)) {
+                    $moduleIndex = array_search($name, $moduleNames);
+                    $enableModule = true;
+                } else {
+                    // is the module already disabled?
+                    $moduleIndex = array_search($name, $enabledModuleNames);
+                    if ($moduleIndex !== false) {
+                        $output->writeln('<info>Module: '.$name.' is enabled</info>');
+                    } else {
+                        $output->writeln('<error>Module: '.$name.' Not found please check input </error>');
+                    }
+                }
+            }
+
+            if ($enableModule) {
+                exec('mv '.$moduleDir.DS.$moduleFilenames[$moduleIndex].' '.$moduleDir.DS.str_replace('.disabled', '', $moduleFilenames[$moduleIndex]));
+                $output->writeln('<info>Enabled '.$moduleNames[$moduleIndex].'</info>');
+            }
         }
     }
 }
